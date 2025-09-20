932b33852e0ffc8f1e95af4f0fbf84713211211d
Rafael <augustico10@hotmail.com>
2025-09-20 12:04:57 -0500

    pagina de register no funcional

--- Wallpapersapp/src/app/pages/register/register.module.ts
+++ Wallpapersapp/src/app/pages/register/register.module.ts
@@ -8,12 +8,14 @@ import { RegisterPageRoutingModule } from './register-routing.module';
 
 import { RegisterPage } from './register.page';
 
+import { SharedModule } from 'src/app/shared/shared-module';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RegisterPageRoutingModule
+    RegisterPageRoutingModule,
+    SharedModule
   ],
   declarations: [RegisterPage]
 })
